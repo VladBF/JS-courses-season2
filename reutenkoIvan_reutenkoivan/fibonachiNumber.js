73197f69839e06dd8db5b192846112af5c8f5c39
reutenkoivan <reutenkoivan@gmail.com>
2018-09-29 21:21:54 +0300

    add index.html and four .js files in which the homework#2 done

---
+++ reutenkoIvan_reutenkoivan/fibonachiNumber.js
@@ -0,0 +1,23 @@
+
+
+function fibo(count){
+	var result = [1, 1];
+	var previous = 1, current = 1;
+
+  for (var i = 3; i <= count; i++) {
+    var sum = previous + current;
+    previous = current;
+    current = sum;
+    result.push(current);
+  }
+
+  return result;
+}
+
+console.log(" ");
+console.log(fibo(15));
+
+
+
+
+
---
+++ reutenkoIvan_reutenkoivan/uniqueArray.js
@@ -0,0 +1,28 @@
+
+function unique(arr) {
+  var result = [];
+
+  nextStep:
+    for (var i = 0; i < arr.length; i++) {
+      var buff = arr[i];
+
+      for (var j = 0; j < result.length; j++) {
+        if (result[j] == buff) {
+        	continue nextStep; 
+        }
+      }
+      result.push(buff);
+    }
+
+  return result;
+}
+
+var numbers = [1,1,2,3,1,2];
+
+console.log(" ");
+console.log(numbers);
+console.log( unique(numbers) ); 
+
+
+
+
