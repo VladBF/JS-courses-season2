cb031e126bc1de5fd68d7a3b4306e22009f8b22f
Maxim Holenko <maxim.ggolenko@gmail.com>
2018-09-30 16:49:45 +0300

    Update uniqueElements.js

--- Maxim Holenko_Max1m1/lesson2/uniqueElements.js
+++ Maxim Holenko_Max1m1/lesson2/uniqueElements.js
@@ -1,19 +1,21 @@
 var arr = [6, 4, 4, 7, 6, 8];
 
-function uniqElements (diffArray) {
-    var n = diffArray.length, k = 0, SortArr = [];
+function uniqElements(diffArray) {
+    var n = diffArray.length;
+    var k = 0;
+    var sortArr = [];
 
     for (var i = 0; i < n; i++) {
         var j = 0;
-        while (j < k && SortArr[j] !== diffArray[i]) {
+        while (j < k && sortArr[j] !== diffArray[i]) {
             j++;
         }
         if (j == k) {
-            SortArr[k++] = diffArray[i];
+            sortArr[k++] = diffArray[i];
         }
     }
 
-    return SortArr.sort((a, b) => a - b);
+    return sortArr.sort((a, b) => a - b);
 }
 
 console.log(uniqElements(arr));
