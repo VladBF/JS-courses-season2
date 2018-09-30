ef5aaeba4872bbf296ac8b220375dbcdce0dd023
Bohdan Kolisnik <43568935+KolissnikBogdan@users.noreply.github.com>
2018-09-30 17:00:37 +0300

    Update numbersFibonnachi.js

--- Bohdan Kolisnik KolissnikBogdan/lesson 2/numbersFibonnachi.js
+++ Bohdan Kolisnik KolissnikBogdan/lesson 2/numbersFibonnachi.js
@@ -3,17 +3,17 @@
  */
 
 function numFibonnachi(coutn) {
-	var sum = 0, arr = [];
+    var sum = 0, arr = [];
 
-	for (let i = 0; i < coutn; i++) {
-		if (arr.length > 1 ) sum += arr[arr.length - 2];
-		if (arr.length == 0) sum = 0;
-		if (arr.length == 1) sum = 1;
-		
-		arr.push(sum);
-	}
+    for (let i = 0; i < coutn; i++) {
+        if (arr.length > 1 ) sum += arr[arr.length - 2];
+        if (arr.length == 0) sum = 0;
+        if (arr.length == 1) sum = 1;
 
-	return arr;
+        arr.push(sum);
+    }
+
+    return arr;
 }
 
 console.log("Array with Fibonacci numbers : ");
