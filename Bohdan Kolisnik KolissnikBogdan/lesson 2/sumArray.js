1f0acfcc11a9d5af5da8129c04431d0039c0318f
Bohdan Kolisnik <43568935+KolissnikBogdan@users.noreply.github.com>
2018-09-30 16:36:43 +0300

    Update sumArray.js

--- Bohdan Kolisnik KolissnikBogdan/lesson 2/sumArray.js
+++ Bohdan Kolisnik KolissnikBogdan/lesson 2/sumArray.js
@@ -4,7 +4,7 @@ var arbitraryArray = [2, 3, 3, 4, 4, 2, 1, 10];
  * Function of finding the sum of the array
  */
 
-function sumArray (someArray) {
+function sumArray(someArray) {
 	var sum = 0;
 
 	for (var i = 0; i < someArray.length; i++) {
