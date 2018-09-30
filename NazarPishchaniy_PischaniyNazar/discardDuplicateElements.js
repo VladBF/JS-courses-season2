e8073a9e645eb21c2348b037fcfe77c696fb4d2e
PischaniyNazar <nazar.psshaniy.96@gmail.com>
2018-09-30 12:30:51 +0300

    add files: summArray.js arraySort.js discardDuplicateElements.js fibonacciNumbers.js

---
+++ NazarPishchaniy_PischaniyNazar/discardDuplicateElements.js
@@ -0,0 +1,16 @@
+//функція видалення не унікальних елементів
+function discardDuplicateElements(array){
+	for(var i=0; i<array.length; i++){
+		for(var j=0; j<array.length; j++){
+			if(i!==j){
+				if(array[i] === array[j]){
+					array.splice(j, 1);
+				}
+			}		
+		}
+	}
+	return array;
+}
+
+var uniqueArray = discardDuplicateElements(array);
+console.log("Масив з унікальними елементами: " + uniqueArray);
