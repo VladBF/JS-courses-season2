7cc88e5e7cf5fbc97d4575c6680bc7837c6682aa
maxwellua <43552751+maxwellua@users.noreply.github.com>
2018-10-01 14:19:43 +0300

    Update arraySortingFor.js

--- MaksimSpilnichenko_maxwellua/lesson2/js/arraySortingFor.js
+++ MaksimSpilnichenko_maxwellua/lesson2/js/arraySortingFor.js
@@ -2,15 +2,15 @@ var someArray = [1,56,3,45,6,110,15,24,2,1];
 
 function sortThisArray(inputArray){
   var swap;
-  for (var i = 0; i<inputArray.length-1; i++) {
-	for (var j = 0; j<inputArray.length-i; j++) {  
-	  if (inputArray[j]>inputArray[j+1]){
-		swap = inputArray[j];
-		inputArray[j] = inputArray[j+1];
-		inputArray[j+1] = swap;
-	  }
+  for (var i = 0; i < inputArray.length-1; i++) {
+		for (var j = 0; j < inputArray.length-i; j++) {  
+			if (inputArray[j] > inputArray[j+1]){
+				swap = inputArray[j];
+				inputArray[j] = inputArray[j+1];
+				inputArray[j+1] = swap;
+			}
+		}
 	}
-  } 
   
   return inputArray;
 }
